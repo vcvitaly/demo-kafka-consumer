7c8b4920463246434e4eff371c45811f781bbe3e
Vitalii Chura <vcvitaly@yahoo.com>
2024-02-13 05:24:52 +0200

    Initial commit

---
+++ src/test/java/io/github/vcvitaly/demokafkaconsumer/DemoKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vcvitaly.demokafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4157966d07baf14d522f8dd3f02bb7e6989cd0bd
Vitalii Chura <vcvitaly@yahoo.com>
2024-02-13 08:34:30 +0200

    Add features and random sleep

--- src/main/java/io/github/vcvitaly/demokafkaconsumer/DemoKafkaConsumerApplication.java
+++ src/main/java/io/github/vcvitaly/demokafkaconsumer/DemoKafkaConsumerApplication.java
@@ -1,25 +1,13 @@
 package io.github.vcvitaly.demokafkaconsumer;
 
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class DemoKafkaConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoKafkaConsumerApplication.class, args);
-	}
-
-	@Bean
-	public ApplicationRunner runner(KafkaTemplate<String, String> template) {
-		return args -> {
-			template.send("topic1", "test");
-		};
 	}
 
 }
--- src/main/java/io/github/vcvitaly/demokafkaconsumer/TestConsumer.java
+++ src/main/java/io/github/vcvitaly/demokafkaconsumer/TestConsumer.java
@@ -1,13 +1,17 @@
 package io.github.vcvitaly.demokafkaconsumer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.vcvitaly.producercommon.TestDto;
 import io.github.vcvitaly.producercommon.TestType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -18,23 +22,43 @@ import java.util.concurrent.atomic.LongAdder;
 public class TestConsumer {
 
     private final Map<Integer, String> db = new ConcurrentHashMap<>();
-    private final LongAdder adder = new LongAdder();
+    private final LongAdder adderCreated = new LongAdder();
+    private final LongAdder adderUpdated = new LongAdder();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Random rand = new Random();
 
     public TestConsumer() {
-        Executors.newSingleThreadScheduledExecutor().schedule(this::printStats, 10, TimeUnit.SECONDS);
+        Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(this::printStats, 0, 10, TimeUnit.SECONDS);
     }
 
-    @KafkaListener(topics = "${kafka.consumer.topic}")
-    public void listen(TestDto testDto) {
-        if (Objects.requireNonNull(testDto.type()) == TestType.CREATE) {
-            create(testDto.id(), testDto.data());
-        } else if (testDto.type() == TestType.UPDATE) {
-            update(testDto.id(), testDto.data());
+    @KafkaListener(id = "${kafka.consumer.id}", topics = "${kafka.consumer.topic}")
+    public void listen(@Payload String s) {
+        try {
+            TestDto testDto = objectMapper.readValue(s, TestDto.class);
+            if (Objects.requireNonNull(testDto.type()) == TestType.CREATE) {
+                run(() -> create(testDto.id(), testDto.data()));
+            } else if (testDto.type() == TestType.UPDATE) {
+                run(() -> update(testDto.id(), testDto.data()));
+            }
+        } catch (Exception e) {
+            log.error("Error: ", e);
+            throw new RuntimeException(e);
         }
+    }
+
+    private CompletableFuture<Void> run(Runnable r) {
+        return CompletableFuture.runAsync(r)
+                .whenComplete((res, e) -> {
+                    if (e != null) {
+                        log.error("Error while running: ", e);
+                    }
+                });
     }
 
     private void create(Integer id, String data) {
         db.put(id, data);
+        sleep(rand);
+        adderCreated.increment();
     }
 
     private void update(Integer id, String data) {
@@ -43,12 +67,23 @@ public class TestConsumer {
         } else {
             log.error("Trying to update non-existent entry with id : %d".formatted(id));
         }
+        sleep(rand);
+        adderUpdated.increment();
+    }
+
+    private void sleep(Random r) {
+        try {
+            Thread.sleep(r.nextInt(5));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void printStats() {
-        long l = adder.sumThenReset();
-        if (l > 0) {
-            log.info("Consumed [%d] messages".formatted(l));
+        long created = adderCreated.sumThenReset();
+        long updated = adderUpdated.sumThenReset();
+        if (created > 0 || updated > 0) {
+            log.info("Consumed [created=%d,updated=%d] messages".formatted(created, updated));
         }
     }
 }
